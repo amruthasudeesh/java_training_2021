aaaba75e5660356e6bc7d13cfd9d045b0186d046
amruthasudheesh <amrutha.sudheesh.k@gmail.com>
2021-07-18 06:56:31 +0530

    math operation added

--- session4/mavenDemo/mavenDemo/src/main/java/org/demo/App.java
+++ session4/mavenDemo/mavenDemo/src/main/java/org/demo/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello java" );
+        MathOperation mathOperation=new MathOperation();
+        System.out.println("Addition of two numbers:"+mathOperation.addOperation(5,6));
+        System.out.println("Subtraction of two numbers:"+mathOperation.subtractOperation(30,5));
+        System.out.println("Remainder of a number:"+new MathHelper().remainder(12,5));
+
     }
 }
